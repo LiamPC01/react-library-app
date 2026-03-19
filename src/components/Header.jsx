9f4d197eac20f90f34155238473570efefdd5486
Liam <liam14mitchell@gmail.com>
2026-03-19 18:22:12 +0000

    experimenting with google books api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,8 +45,8 @@ export default function Header() {
             <section className="searchbar-section">
                 <div className="header-section-content">
                     <div className="input-container">
-                        <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke={searchSelected ? "tomato" : "grey"} >
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                        <svg className="search-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke={searchSelected ? "tomato" : "grey"} >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
                         <input type="text" className="searchbar" placeholder="Search for a book"
                             onFocus={() => setSearchSelected(true)}
--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,4 +1,31 @@
+import { useState, useEffect } from "react"
+
 export default function Library() {
+
+    const [books, setBooks] = useState([])
+
+    function addBookFromURL(url) {
+        useEffect(() => {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    const book = data.items?.[0]?.volumeInfo;
+                    setBooks({...books, book})
+                    console.log(book)
+                });
+        }, [])
+    }
+
+    addBookFromURL("https://www.googleapis.com/books/v1/volumes?q=deep+work")
+    addBookFromURL("https://www.googleapis.com/books/v1/volumes?q=isbn:9780451524232")
+    addBookFromURL("https://www.googleapis.com/books/v1/volumes?q=after+the+ice")
+    addBookFromURL("https://www.googleapis.com/books/v1/volumes?q=isbn:9780571192588")
+
+console.log("books array:" + books)
+
+
+// above code is temporary, useffect should not be inside a function
+    
     return (
         <section>
             <p>Library Component</p>
