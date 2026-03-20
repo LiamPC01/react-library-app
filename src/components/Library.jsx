880621d6d1750ec38f257cda98e41b9a359e313e
Liam <liam14mitchell@gmail.com>
2026-03-20 17:43:41 +0000

    some books added manually to the homepage and styling added for them. some state added for the beginnings of what will be the search function

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -2,34 +2,77 @@ import { useState, useEffect } from "react"
 
 export default function Library() {
 
-    const [books, setBooks] = useState([])
-
-    function addBookFromURL(url) {
-        useEffect(() => {
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    const book = data.items?.[0]?.volumeInfo;
-                    setBooks({...books, book})
-                    console.log(book)
-                });
-        }, [])
-    }
-
-    addBookFromURL("https://www.googleapis.com/books/v1/volumes?q=deep+work")
-    addBookFromURL("https://www.googleapis.com/books/v1/volumes?q=isbn:9780451524232")
-    addBookFromURL("https://www.googleapis.com/books/v1/volumes?q=after+the+ice")
-    addBookFromURL("https://www.googleapis.com/books/v1/volumes?q=isbn:9780571192588")
-
-console.log("books array:" + books)
+    const [defaultBooksData, setdefaultBooksData] = useState([
+        {
+            title: "Deep Work",
+            author: "Cal Newport",
+            subtitle: "Rules for Focused Success in a Distracted World",
+            thumbnail: "https://books.google.com/books/content?id=lZpFCgAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+            id: "001"
 
 
-// above code is temporary, useffect should not be inside a function
-    
+        },
+        {
+            title: "After The Ice",
+            author: "Steven Mithen",
+            subtitle: "A Global Human History, 20,000-5000 BC",
+            thumbnail: "http://books.google.com/books/content?id=fBJ9AAAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+            id: "002"
+        },
+        {
+            title: "Tarzan of the Apes",
+            author: "Edgar Rice Burroughs",
+            subtitle: "tarzan Description blah blah blah",
+            thumbnail: "https://books.google.com/books/content?id=2NRFq7hw4f0C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+            id: "003"
+        },
+        {
+            title: "The Inheritors",
+            author: "William Golding",
+            subtitle: "Inheritors description blah blah blah",
+            thumbnail: "https://books.google.com/books/content?id=Ok-DaBj7GqMC&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+            id: "004"
+        }
+    ])
+
+    const [searchQuery, setSearchQuery] = useState("") //update search query each keystroke
+    const [searchResults, setSearchResults] = useState([]) //store x amount of books from api to be rendered if searchquery is not empty
+
+
+    // console.log("Rendered!")
+    // useEffect(() => {
+    //     fetch("https://www.googleapis.com/books/v1/volumes?q=deep+work&key=AIzaSyAFh3jqb7IGPoTrh4q8q1WrVOuFmQsvdis")
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    // },[])
+
+    const defaultBooks = defaultBooksData.map(book => (
+        <div className="book-card" key={book.id}>
+            <img src={book.thumbnail}></img>
+            <div className="book-card-text">
+                <h2>{book.title}</h2>
+                <p className="author">{book.author}</p>
+                <p>{book.subtitle}</p>
+            </div>
+
+
+        </div>
+    ))
+
+    // render an array of books called editors picks if there is no input within the search bar
+    // if there is data in the search bar then the title should change to (search result for "search") and the books rendered should be some of the items returned from the api
+    //editors picks does not need to be in state as it does not change?
+    //search results array that contains say 10 results
     return (
-        <section>
-            <p>Library Component</p>
-        </section>
+        <>
+            {!searchQuery && <div>
+                <h1> Editors Picks</h1>
+                <section className="books-card-container">
+                    {defaultBooks}
+                </section>
+            </div>}
+        </>
+
 
 
     )
